21c3e5ac2c88a7ed5fc5e3a9d5da67600bfa6634
PiotrMa <piotrmgrupa4@gmail.com>
2017-06-22 23:30:14 +0200

    Elimination strategy implemented

--- src/Utils.java
+++ src/Utils.java
@@ -1,6 +1,8 @@
+
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 import java.util.stream.Stream;
 
@@ -24,6 +26,7 @@ public class Utils {
 		int[] rowBoxValues = {-1,-1,-1}; 
 		int[] colBoxValues = {-1,-1,-1};
 		String[] result = new String[8];
+		
 		if(index2D[0]==-1){
 			return null;
 		}else{
@@ -173,12 +176,12 @@ public class Utils {
                 .toArray(String[]::new);
 		for (int i = 0; i < tempPositions.length; i++) {
 			if(board.getBoard()[Integer.parseInt(tempPositions[i].substring(0, 1))][Integer.parseInt(tempPositions[i].substring(1))]!=(-1)) {
-				System.out.print(board.getBoard()[Integer.parseInt(tempPositions[i].substring(0, 1))][Integer.parseInt(tempPositions[i].substring(1))]+" ");
+				//System.out.print(board.getBoard()[Integer.parseInt(tempPositions[i].substring(0, 1))][Integer.parseInt(tempPositions[i].substring(1))]+" ");
 				tempList.remove(new Integer(board.getBoard()[Integer.parseInt(tempPositions[i].substring(0, 1))][Integer.parseInt(tempPositions[i].substring(1))]));
 			}
 				
 		}
-		System.out.println(tempList.toString());
+		//System.out.println(tempList.toString());
 		return tempList;
 		
 	}
@@ -200,7 +203,8 @@ public class Utils {
 	}
 	
 	public void createTreeOfSolutions() {
-		ArrayList<Integer> tempArray;
+		possibleSolutionsMap = new HashMap<>();
+		ArrayList<Integer> tempArray = new ArrayList<>();
 		for(int i = 0; i<9; i++) {
 			for(int j = 0; j<9; j++) {
 				if(board.getPositionValue(i, j)==(-1)) {
@@ -211,7 +215,7 @@ public class Utils {
 						board.reduceEmptyPositionCounter();
 					}else {
 						possibleSolutionsMap.put(i+""+j+"",tempArray);
-
+						//System.out.println("["+i+"]["+j+"]"+possibleSolutionsMap.get(i+""+j));
 					}
 					
 				}
@@ -219,38 +223,149 @@ public class Utils {
 			}
 		}
 		
-		board.printBoard();
+		
 	}
 	
 	public void onlyChoiceSolutionBox(String position) {
-		System.out.println("!!!"+position);
-		String[] tempBox = getBox(position);
-		int[] index2D = board.getPositionIndex2D(position);
-		System.out.println("!!"+index2D[0]+index2D[1]);
-		ArrayList<Integer> tempValueList = new ArrayList<>(Arrays.asList(1,2,3,4,5,6,7,8,9));
-		ArrayList<Integer> repeateValueList = new ArrayList<>();
-		if(board.getPositionValue(position)==(-1)){
-			//System.out.print(possibleSolutionsMap.get(index2D).toString());
-			System.out.println(index2D[0]+""+index2D[1]+" "+possibleSolutionsMap.get(index2D[0]+index2D[1]+""));
-		}else{
-			tempValueList.remove(board.getPositionValue(position));
-		}
-		for(int i =0; i< tempBox.length; i++){
-			if(board.getPositionValue(Integer.parseInt(tempBox[i].substring(0, 1)),Integer.parseInt(tempBox[i].substring(1)))==(-1)){
-				System.out.print(tempBox[i]+" "+possibleSolutionsMap.get(tempBox[i]+"")+" ");
-				for(Integer val : possibleSolutionsMap.get(tempBox[i]+"")){
-					if(repeateValueList.contains(val)){
-						tempValueList.remove(val);
-					}else{
-						repeateValueList.add(val);
+		
+		//System.out.println(possibleSolutionsMap.size());
+		if(possibleSolutionsMap.size()>0) {
+			String[] tempBox = getBox(position);
+			
+			ArrayList<ArrayList<Integer>> tempPossibleValues = new ArrayList<>();
+			ArrayList<int[]> tempValPosIndex = new ArrayList<>();
+			
+			int[] index2D = board.getPositionIndex2D(position);
+			if(board.getPositionValue(index2D[0],index2D[1])==(-1)) {
+				ArrayList<Integer> tempPositionSolutions = possibleSolutionsMap.get(index2D[0]+""+index2D[1]);
+				tempPossibleValues.add(tempPositionSolutions);
+				tempValPosIndex.add(index2D);
+			}
+			for(int i =0; i<tempBox.length; i++) {
+				index2D = new int[]{Integer.parseInt(tempBox[i].substring(0, 1)),Integer.parseInt(tempBox[i].substring(1))};
+				
+				if(board.getPositionValue(index2D[0],index2D[1])==(-1)) {
+					ArrayList<Integer> tempPositionSolutions = possibleSolutionsMap.get(index2D[0]+""+index2D[1]);
+					
+					tempPossibleValues.add(tempPositionSolutions);
+					tempValPosIndex.add(index2D);
+				}
+			}
+			
+			
+			int[] repeateValuesCountList = new int[10];
+			
+			for(int i=0; i<tempPossibleValues.size(); i++) {
+				for(int val : tempPossibleValues.get(i)) {
+					repeateValuesCountList[val]++;
+				}
+				
+			}
+			
+			//System.out.println("");
+			for(int i=0; i<repeateValuesCountList.length; i++) {
+				if(repeateValuesCountList[i]==1) {
+					for(int j=0; j<tempPossibleValues.size(); j++) {
+						for(int val : tempPossibleValues.get(j)) {
+							//System.out.println("VAL: "+val+" repe "+i);
+							if(val==i) {
+								board.setBoardValue(tempValPosIndex.get(j)[0], tempValPosIndex.get(j)[1], i);
+								repeateValuesCountList[i]=0;
+							}
+						}
+						
 					}
 				}
-			}else{
-				//System.out.println(tempValueList.contains(board.getPositionValue(Integer.parseInt(tempBox[i].substring(0, 1)),Integer.parseInt(tempBox[i].substring(1)))));
-				//tempValueList.remove(board.getPositionValue(Integer.parseInt(tempBox[i].substring(0, 1)),Integer.parseInt(tempBox[i].substring(1))));
+			}
+			//createTreeOfSolutions();
+			
+		}
+	}
+	
+	public void onlyChoiceSolutionBox(int row, int col) {
+			
+			if(possibleSolutionsMap.size()>0) {
+				String[] tempBox = getBox(row, col);
+				
+				Map<int[],ArrayList<Integer>> tempValMapPos = new HashMap<>();
+				ArrayList<ArrayList<Integer>> tempPossibleValues = new ArrayList<>();
+				ArrayList<int[]> tempValPosIndex = new ArrayList<>();
+				int[] reservedValues = new int[9]; 
+				int indexReservedValues = 0;
+				
+				int[] index2D = new int[] {row, col};
+				if(board.getPositionValue(index2D[0],index2D[1])==(-1)) {
+					ArrayList<Integer> tempPositionSolutions = possibleSolutionsMap.get(index2D[0]+""+index2D[1]);
+					//tempValMapPos.put(index2D,tempPositionSolutions);
+					tempPossibleValues.add(tempPositionSolutions);
+					tempValPosIndex.add(index2D);
+				}else {
+					reservedValues[indexReservedValues]=board.getPositionValue(index2D[0],index2D[1]);
+					indexReservedValues++;
+				}
+				
+				for(int i =0; i<tempBox.length; i++) {
+					index2D = new int[]{Integer.parseInt(tempBox[i].substring(0, 1)),Integer.parseInt(tempBox[i].substring(1))};
+					
+					if(board.getPositionValue(index2D[0],index2D[1])==(-1)) {
+						ArrayList<Integer> tempPositionSolutions = possibleSolutionsMap.get(index2D[0]+""+index2D[1]);
+						
+						tempPossibleValues.add(tempPositionSolutions);
+						tempValPosIndex.add(index2D);
+					}else {
+						reservedValues[indexReservedValues]=board.getPositionValue(index2D[0],index2D[1]);
+						indexReservedValues++;
+					}
+				}
+				
+				
+				
+				
+				int[] repeateValuesCountList = new int[10];
+				
+				for(int i=0; i<repeateValuesCountList.length; i++) {
+					for(int h : reservedValues) {
+						if(i==h) {
+							repeateValuesCountList[i]=0;
+						}
+					}
+				}
+				
+				for(int i=0; i<repeateValuesCountList.length; i++) {
+					if(repeateValuesCountList[i]==1) {
+						for(int j=0; j<tempPossibleValues.size(); j++) {
+							for(int val : tempPossibleValues.get(j)) {
+								//System.out.println("VAL: "+val+" repe "+i);
+								if(val==i) {
+									boolean isPossible = true;
+									for(int reservedVal : reservedValues) {
+										if(reservedVal==val) {
+											isPossible=false;
+										}
+									}
+									if(isPossible)
+										board.setBoardValue(tempValPosIndex.get(j)[0], tempValPosIndex.get(j)[1], i);
+								}
+							}
+							
+						}
+					}
+				}
+				//createTreeOfSolutions();
+				
 			}
 		}
+
+	public void ultimateSolver() {
+		createTreeOfSolutions();
+		for(int i =0; i<7;i+=3) {
+			for(int j = 0; j<7; j+=3) {
+				onlyChoiceSolutionBox(i, j);
+				//System.out.println(i+" "+j);
+			}
+		}
+		//System.out.println(board.returnEmptyPositionCounter());
+		//board.printBoard();
 		
-		System.out.println("ARR "+tempValueList.toString());
 	}
 }
--- src/app.java
+++ src/app.java
@@ -1,7 +1,9 @@
 
 public class app {
 	public static void main(String[] args) {
-		Board sudoku = new Board("..3.2.6..9..3.5..1..18.64....81.29..7.......8..67.82....26.95..8..2.3..9..5.1.3..");
+		//Board sudoku = new Board("..3.2.6..9..3.5..1..18.64....81.29..7.......8..67.82....26.95..8..2.3..9..5.1.3..");
+		//Board sudoku = new Board("7...3...1...178.....5...2...26...49...........84...72...3...6.....359...4...1...9"); //hard
+		Board sudoku = new Board("..3.92...4...3..1.27........1.3....8.5.167.3.3....8.6........53.3..8...9...62.1..");
 		sudoku.printBoard();
 		/*sudoku.getBox("H7");
 		sudoku.getLines("H7").toString();*/
@@ -12,12 +14,34 @@ public class app {
 		utils.createTreeOfSolutions();
 		System.out.println(sudoku.returnEmptyPositionCounter());*/
 		int counter = 0;
-		while(sudoku.returnEmptyPositionCounter()>0){
-			utils.createTreeOfSolutions();
+/*		while(sudoku.returnEmptyPositionCounter()>0){
+			utils.ultimateSolver();
 			counter++;
 		}
-		System.out.println(counter);
-		//utils.onlyChoiceSolutionBox("I9");
+		System.out.println(counter);*/
+		//utils.createTreeOfSolutions();
+		//utils.onlyChoiceSolutionBox(0,3);
 		
+		//sudoku.printBoard();
+		//utils.createTreeOfSolutions();
+		
+/*		utils.createTreeOfSolutions();
+		sudoku.printBoard();
+		utils.ultimateSolver();
+		sudoku.printBoard();
+		utils.ultimateSolver();
+		sudoku.printBoard();*/
+		utils.ultimateSolver();
+		utils.ultimateSolver();
+		utils.ultimateSolver();
+		utils.ultimateSolver();
+		utils.ultimateSolver();
+		utils.ultimateSolver();
+		utils.ultimateSolver();
+		sudoku.printBoard();
+
+		//System.out.println(sudoku.returnEmptyPositionCounter());
+
+
 	}
 }
